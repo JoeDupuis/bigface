c00e0ca1811b56ce3d23ed1a4975f922f1637851
Joé Dupuis <joe@dupuis.io>
2026-01-02 14:44:30 -0800

    Replace Capybara/Selenium with Cuprite for system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,15 @@
 require "test_helper"
 
+Capybara.test_id = "data-qa"
+Capybara.add_selector(:test_id) do
+  xpath do |locator|
+    XPath.descendant[XPath.attr(Capybara.test_id) == locator]
+  end
+end
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  driven_by :cuprite, using: :chrome, screen_size: [ 1400, 1400 ], options: {
+    headless: true,
+    process_timeout: 20
+  }
 end
